a4e815291244f043e32fa6b92985f068de11de16
Iloba <scarlettsney@gmail.com>
2021-02-20 20:20:38 +0100

    FIRST COMMIT

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Teworh Foundation</title>
+	 <link rel="icon" href="img/logo.jpg">
+	<title>P & A Football Academy</title>
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="Emeka Iloba Timothy">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="imagetoolbar" content="no">
+
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="style/style.css">
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+
+
+	
+</head>
+<body>
+		<nav class=" height sticky-top navbar navbar-expand-lg navbar-light bg-light shadow">
+	        <a style="margin-left: 60px;" class="navbar-brand" id="brand" href="index.php"><img draggable="false" class="navbar-brand" style="width: 40%" src="img/logo.jpg"></a>
+	        <button class="navbar-toggler bg-white" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="					navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	              <span class="navbar-toggler-icon"></span>
+	        </button>
+	        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+	        <ul class="navbar-nav ml-auto">
+	           <!--  <li class="nav-item">
+	                <a  id="nav_white" class="nav-link"   href="index.php">Home <span class="sr-only">(current)</span></a>
+	            </li> -->
+	            <li class="nav-item">
+	                 <a class="nav-link"   href="#">About Us</a>
+	            </li>
+	            <li class="nav-item">
+	                <a class="nav-link"   href="#">Products/Services</a>
+	            </li>
+	            <li class="nav-item">
+	                <a class="nav-link"   href="#">Football Academy</a>
+	            </li>
+	             <li class="nav-item">
+	                <a class="nav-link"   href="#">Gallery</a>
+	            </li>
+	             <li class="nav-item">
+	                <a class="nav-link"   href="#">Support</a>
+	            </li>
+	            <li class="nav-item nav-btn-two">
+	                <a class=" btn btn-success"   href="#">Shop</a>
+	            </li> 
+	             <li class="nav-item nav-btn">
+	                <a class=" btn btn-danger"   href="#">Connect with Us</a>
+	            </li>
+
+	        </ul>
+	    </div>
+ 	 </nav>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
