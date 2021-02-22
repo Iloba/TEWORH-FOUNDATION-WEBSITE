18c5ca7b25fcd6803e42afdddf4cb3dec95fa56f
Iloba <scarlettsney@gmail.com>
2021-02-22 21:46:04 +0100

    good

---
+++ about.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Teworh Foundation</title>
+	 <link rel="icon" href="img/logo.jpg">
+	<title>P & A Football Academy</title>
+    <meta charset="utf-8">
+    <meta name="description" content="Teworh Foundation is a Non Governmental Organisation that seeks to train and raise a new set of purpose oriented youths in our society through sports and skill aquisition.
+	we are non-profit">
+    <meta name="keywords" content=" teworh foundation, #teworhfoundation Foundation in Kaduna Football academy in Kaduna, Helping the needy in Kaduna, Youths and Sports in Kaduna, Children & Youth Foundation">
+    <meta name="author" content="Emeka Iloba Timothy">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="imagetoolbar" content="no">
+
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="style/style.css">
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
+
+	
+</head>
+<body>
+		<nav class=" height sticky-top navbar navbar-expand-lg navbar-light bg-light shadow">
+	        <a class="navbar-brand" id="brand" href="index.php"><img draggable="false" class="navbar-brand" style="width: 40%" src="img/logo.jpg"></a>
+	        <button class="navbar-toggler bg-white" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="					navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	              <span class="navbar-toggler-icon"></span>
+	        </button>
+	        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+	        <ul class="navbar-nav ml-auto">
+	            <li class="nav-item">
+	                 <a class="nav-link"   href="about.php">About us</a>
+	            </li>
+	            <li class="nav-item">
+	                <a class="nav-link"   href="services.php">Products/Services</a>
+	            </li>
+	            <li class="nav-item">
+	                <a class="nav-link"   href="fa.php">Football Academy</a>
+	            </li>
+	             <li class="nav-item">
+	                <a class="nav-link"   href="gallery.php">Gallery</a>
+	            </li>
+	           <!--   <li class="nav-item">
+	                <a class="nav-link"   href="#">Support</a>
+	            </li> -->
+	            <li class="nav-item nav-btn-two">
+	                <a class=" btn btn-success"   href="contact.php">Connect with Us</a>
+	            </li> 
+	             <li class="nav-item nav-btn">
+	                <a class=" btn btn-danger"   href="#">Support/Donate</a>
+	            </li>
+
+	        </ul>
+	    </div>
+ 	 </nav>
+ 	 <div  class="first-section">
+ 	 		<div class="container">
+ 	 			<div class="row">
+ 	 				<div class="col-md-12">
+ 	 					<div class="welcome" data-aos="fade-left"  data-aos-duration="3000">
+ 	 						<h1 class="text-center teworh">Our Vison and Mission</h1>
+ 	 						<h4 class="text-center all-headers">Our Vision is to help the society become a better entity by the use of Sports, Sensitization and provision of welfare for the masses</h4>
+		 	 				
+ 	 					</div>
+ 	 					
+ 	 				</div>
+ 	 			</div>
+ 	 		</div>
+ 	 </div>
+
+ 	 <div class="second-section" data-aos="fade-up" data-aos-duration="2000">
+ 	 		<div class="container">
+ 	 			<div class="row">
+ 	 					<div class="col-md-12">
+ 	 						<h1 class="text-success text-center all-headers">Who we are</h1>
+ 	 						<p class=" text-center welcome-text">The Teworh Foundation is the leading champion of Empowerment in Nigeria. Our objective is to empower women and men across our Nation using Sports,  driving poverty eradication and providing for the less priviledged. We believe that every Child, youth, man or woman deserves to live a better life and Grow to Impact their world</p>
+ 	 					</div>
+ 	 			</div>
+ 	 		</div>
+ 	 </div>
+
+ 	 <div class="second-section" data-aos="fade-up" data-aos-duration="2000">
+ 	 		<div class="container">
+ 	 			<div class="row">
+ 	 					<div class="col-md-6">
+ 	 						<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+							  <ol class="carousel-indicators">
+							    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+							    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+							    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+							  </ol>
+							  <div class="carousel-inner">
+							    <div class="carousel-item active">
+							      <img class="d-block w-100" src="img/teworh-2.jpg" alt="First slide">
+							      <div class="carousel-caption d-none d-md-block">
+								    <h4 class="all-headers">Badmiton</h4>
+								  </div>
+							    </div>
+							    <div class="carousel-item">
+							      <img class="d-block w-100" src="img/teworh-3.jpg" alt="Second slide">
+							       <div class="carousel-caption d-none d-md-block">
+								    <h4 class="all-headers">Football</h4>
+								  </div>
+							    </div>
+							    <div class="carousel-item">
+							      <img class="d-block w-100" src="img/teworh-5.jpg" alt="Third slide">
+							      <div class="carousel-caption d-none d-md-block">
+								    <h4 class="all-headers">Table Tennis</h4>
+								  </div>
+							    </div>
+							  </div>
+							  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+							    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+							    <span class="sr-only">Previous</span>
+							  </a>
+							  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+							    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+							    <span class="sr-only">Next</span>
+							  </a>
+							</div>
+ 	 					</div>
+
+ 	 					<div class="col-md-6">
+ 	 						<h3 class="text-success all-headers">USING SPORTS TO EMPOWER THE YOUTHS</h3>
+ 	 						<p class="welcome-text">Sports is a vital Part and Ingredient of Societal Unity, thats why Teworh Foundation is utilizing the tool to help Kids, Teenagers and Young Adults come together, unite and Actualize their dreams. We belive that using sports as a tool, Drug abuse, Smoking and other Bad Societal Vices will be dealt with.</p>
+ 	 						<div id="one-alone" class="two-buttons">
+		 	 					<a class="btn btn-danger" href="fa.php">Learn More</a> 
+		 	 				</div>
+ 	 					</div>
+ 	 					
+ 	 				</div>
+ 	 			</div>
+ 		 </div>
+
+ 		<div  class="first-section" id="fa-section">
+ 	 		<div class="container">
+ 	 			<div class="row">
+ 	 				<div class="col-md-12">
+ 	 					<div class="welcome" data-aos="fade-left"  data-aos-duration="3000">
+ 	 						<h1 class="text-center teworh">Teworh Football Academy</h1>
+ 	 						<h2 class="text-center">Empowering the Youths Through Sport</h2>
+		 	 				<p class="text-center">Our Football Academy Established in 2016 has been helping over 200 Teenagers around the nation</p>
+		 	 				<div class="two-buttons">
+		 	 					<a class="btn btn-danger" href="fa.php">Go to Football Academy</a> 
+		 	 				</div>
+ 	 					</div>
+ 	 					
+ 	 				</div>
+ 	 			</div>
+ 	 		</div>
+ 	 </div>
+
+ 	 
+
+
+ 	 <footer data-aos="fade-up" data-aos-duration="2000" class="bg-light">
+ 	 	<div class="container-fluid">
+ 	 		<div class="container">
+ 	 			<div class="row">
+	 	 			<div class="col-md-4">
+	 	 				<h3 class="text-success">Visit Us</h3>
+	 	 				<p>
+	 	 					14, Worshippers Close Malali, Kaduna, Kaduna State
+	 	 					<br>
+	 	 					<span class="text-success">Call us: + 234 (0) 902 403 4028</span> <br>
+	 	 					<span class="text-success">Email: <a href="mailto:teworhfoundation@gmail.com">teworhfoundation@gmail.com</a></span>
+	 	 				</p>
+	 	 			</div>
+	 	 			<div class="col-md-4">
+	 	 				<h3 class="text-success">About Us</h3>
+	 	 				<p>
+	 	 					<a target="_blank" class="nav-link" href="about.php">Vison and Mission</a>
+	 	 					<a target="_blank" class="nav-link" href="about.php">Address</a>
+	 	 					<a target="_blank" class="nav-link" href="about.php">Contact us</a>
+	 	 					<a target="_blank" class="nav-link" href="services.php">Services</a>
+	 	 				</p>
+	 	 			</div>
+	 	 			<div class="col-md-4">
+	 	 				<h3 class="text-success">Teworh Football Academy</h3>
+	 	 				<p>Join our football Academy today for  life changing environment of fun and personal development <br> <br>
+	 	 					<a href="fa.php" class="btn btn-danger">Visit Football Academy</a>
+	 	 				</p>
+	 	 			</div>
+ 	 			</div>
+ 	 		</div>
+ 	 		<hr>
+ 	 		<div class="footer-social-media">
+ 	 			<p>
+ 	 				<a target="_blank" href="https://www.instagram.com/teworhsccf/"><img src="img/instagram-svg.svg"></a>
+ 	 				<a target="_blank" href="#"><img src="img/linked-in.svg"></a>
+ 	 				<a target="_blank"  href="facebook.com/Teworh-Foundation-108909477432830/"><img  src="img/facebook.svg"></a>
+ 	 				<a target="_blank"  href="https://www.youtube.com/channel/UC1khibMPPNCeJW4xltnz6xw"><img  src="img/youtube.svg"></a> |
+ 	 				<a target="_blank" href="https://sney.netlify.app">Website Design</a> | 
+ 	 				<a>&copy;2021</a>
+ 	 			</p> 
+ 	 			
+ 	 		</div>
+ 	 	</div>
+ 	 </footer>
+
+ 	 <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	<script>
+  		AOS.init();
+	</script>
+</body>
+</html>
