ed9c1daaa59a300c9980b61e30c0d713bb163017
Iloba <scarlettsney@gmail.com>
2021-03-13 12:04:12 +0100

    Comit

--- gallery.php
+++ gallery.php
@@ -17,6 +17,10 @@
     <script type="text/javascript" src="js/bootstrap.js"></script>
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 
+    <link rel="stylesheet" href="chocolat.css">
+	<script src="chocolat.js"></script>
+
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.0.0/magnific-popup.min.css">
 
 	
 </head>
@@ -25,10 +29,24 @@
  	 <div  class="first-section">
  		<div class="container">
  			<div class="row">
- 				<div class="col-md-8 mx-auto">
+ 				<div class="col-md-11 mx-auto">
  					<div class="" data-aos="fade-left"  data-aos-duration="3000">
 						<div class="gallery-section">
-							<h1 class="all-headers">Gallery Section</h1>
+							<h1 class="all-headers text-center">Gallery Section</h1>
+							<div class="row no-gutters gallerys">
+								<div class="col-md-3  gallerys">
+									<a target="_blank" href="img/teworh-1.jpg"><img class="img-fluid" src="img/teworh-1.jpg"></a>
+								</div>
+								<div class="col-md-3">
+									<a target="_blank" href="img/teworh-1.jpg"><img class="img-fluid" src="img/teworh-1.jpg"></a>
+								</div>
+								<div class="col-md-3">
+									<a target="_blank" href="img/teworh-1.jpg"><img class="img-fluid" src="img/teworh-1.jpg"></a>
+								</div>
+								<div class="col-md-3">
+									<a target="_blank" href="img/teworh-1.jpg"><img class="img-fluid" src="img/teworh-1.jpg"></a>
+								</div>
+							</div>
 						</div> 						
  					</div> 
  				</div>
@@ -37,13 +55,23 @@
  	 </div> 
  	
  	 <?php require 'includes/footer.php'; ?>
-
+ 	 <script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.0.0/jquery.magnific-popup.js"></script>
  	 <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/jquery-3.6.0.slim.min.js" integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI=" crossorigin="anonymous"></script>
 	<script>
   		AOS.init();
+	</script>
+
+	<script>
+		$(document).ready(function(){
+			$('.gallerys').magnificPopup({
+				type:'image',
+				delegate: 'a'
+			})
+		});
 	</script>
 </body>
 </html>
