12defa80f1c27beef3dce4b8230e6a8361a7cf3d
Iloba <scarlettsney@gmail.com>
2021-03-13 09:50:45 +0100

    gallery page

---
+++ gallery.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Teworh Foundation</title>
+	 <link rel="icon" href="img/logo.jpg">
+	<title>P & A Football Academy</title>
+    <meta charset="utf-8">
+    <meta name="description" content="Teworh Foundation is a Non Governmental Organisation that seeks to train and raise a new set of purpose oriented youths in our society through sports and skill aquisition.
+	we are non-profit">
+    <meta name="keywords" content=" teworh foundation, #teworhfoundation Foundation in Kaduna Football academy in Kaduna, Helping the needy in Kaduna, Youths and Sports in Kaduna, Children & Youth Foundation">
+    <meta name="author" content="Emeka Iloba Timothy">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="imagetoolbar" content="no">
+
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="style/style.css">
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
+
+	
+</head>
+<body>
+		<?php require 'includes/header.php'; ?>
+ 	 <div  class="first-section">
+ 		<div class="container">
+ 			<div class="row">
+ 				<div class="col-md-8 mx-auto">
+ 					<div class="" data-aos="fade-left"  data-aos-duration="3000">
+						<div class="gallery-section">
+							<h1 class="all-headers">Gallery Section</h1>
+						</div> 						
+ 					</div> 
+ 				</div>
+ 			</div>
+ 		</div>
+ 	 </div> 
+ 	
+ 	 <?php require 'includes/footer.php'; ?>
+
+ 	 <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	<script>
+  		AOS.init();
+	</script>
+</body>
+</html>
